59960f840a878352e4ee813c01615b1b6c5f8224
Devika <devikab2021@gmail.com>
2022-09-05 20:10:49 +0530

    Displays the uploaded image

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
 from flask import Flask,request, url_for, redirect, render_template
-
+import os
+import sys
+from flask import request
+from random import randint
+from werkzeug.utils import secure_filename
+# from flask.ext.session import Session
 
 app = Flask(__name__)
+# sess = Session()
 
-@app.route('/')
-def hlo_world():
-    return render_template("index.html")
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'log'])
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hloworld():
+    return render_template('index.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    
+    # check if the post request has the file part
+    if 'file' not in request.files:        
+        result = {
+            'result' : 0,    
+            'error' : 'file not available',
+        }
+        return render_template('result.html', result=result)
+    
+    file = request.files['file']
+    
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':        
+        result = {
+            'result' : 0,    
+            'error' : 'file not available',
+        }
+        return render_template('result.html', result=result)
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        print('filepath : ', filepath)
+        
+        result = {
+            'result' : 1,
+            'error' : '0',
+            'image_location' : filepath
+        }
+        return render_template('result.html', result = result, filepath = filepath)
+    
+    #return content
+    return render_template('result.html', user=user)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('IP', '127.0.0.1')
+    port = int(os.environ.get('PORT', 6021))
+    
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    # sess.init_app(app)
+    
+    app.run(host= host, port = port, use_reloader = False)
+    
--- colour.py
+++ colour.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+# from PIL import Image
 
 
 # Kmeans color segmentation
@@ -27,117 +27,117 @@ def kmeans_color_quantization(image, clusters=8, rounds=1):
     return res.reshape((image.shape))
 
 #Read Image
-
-img = cv2.imread('default3.jpg')
-original = img.copy()
-kmeans = kmeans_color_quantization(img, clusters=4)
-
-
-
-# Convert to grayscale, Gaussian blur, adaptive threshold
-gray = cv2.cvtColor(kmeans, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, (3,3), 0)
-thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,21,2)
+def show_colours():
+    img = cv2.imread('default3.jpg')
+    original = img.copy()
+    kmeans = kmeans_color_quantization(img, clusters=4)
 
 
-# Draw largest enclosing circle onto a mask
-mask = np.zeros(original.shape[:2], dtype=np.uint8)
-cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-for c in cnts:
-    ((x, y), r) = cv2.minEnclosingCircle(c)
-    cv2.circle(img, (int(x), int(y)), int(r), (36, 255, 12), 2)
-    cv2.circle(mask, (int(x), int(y)), int(r), 255, -1)
-    break
+
+    # Convert to grayscale, Gaussian blur, adaptive threshold
+    gray = cv2.cvtColor(kmeans, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3,3), 0)
+    thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,21,2)
 
 
-# Bitwise-and for result
-result = cv2.bitwise_and(original, original, mask=mask)
-result[mask==0] = (255,255,255)
+    # Draw largest enclosing circle onto a mask
+    mask = np.zeros(original.shape[:2], dtype=np.uint8)
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+    for c in cnts:
+        ((x, y), r) = cv2.minEnclosingCircle(c)
+        cv2.circle(img, (int(x), int(y)), int(r), (36, 255, 12), 2)
+        cv2.circle(mask, (int(x), int(y)), int(r), 255, -1)
+        break
+
+
+    # Bitwise-and for result
+    result = cv2.bitwise_and(original, original, mask=mask)
+    result[mask==0] = (255,255,255)
 
 
 # Convert from BGR to RGB
 
-img=cv2.cvtColor(result,cv2.COLOR_BGR2RGB)
+    img=cv2.cvtColor(result,cv2.COLOR_BGR2RGB)
 
-# Convert from RGB to HSV
+    # Convert from RGB to HSV
 
-img_HSV = cv2.cvtColor(img , cv2.COLOR_RGB2HSV)
+    img_HSV = cv2.cvtColor(img , cv2.COLOR_RGB2HSV)
 
 # HSV Ranges for DIfferent Colors
 
-redlower = np.array([0,150,50])
-redupper = np.array([10,255,255])
+    redlower = np.array([0,150,50])
+    redupper = np.array([10,255,255])
 
-yellowlower = np.array([25,150,50])
-yellowupper = np.array([35,255,255])
+    yellowlower = np.array([25,150,50])
+    yellowupper = np.array([35,255,255])
 
-greenlower = np.array([35,100,50])
-greenupper = np.array([70,255,255])
+    greenlower = np.array([35,100,50])
+    greenupper = np.array([70,255,255])
 
-bluelower = np.array([100,150,0])
-blueupper = np.array([140,255,255])
+    bluelower = np.array([100,150,0])
+    blueupper = np.array([140,255,255])
 
-orangelower = np.array([15,150,0])
-orangeupper = np.array([25,255,255])
+    orangelower = np.array([15,150,0])
+    orangeupper = np.array([25,255,255])
 
-blacklower = np.array([0,0,0])
-blackupper = np.array([250,255,30])
+    blacklower = np.array([0,0,0])
+    blackupper = np.array([250,255,30])
 
-whitelower = np.array([0,0,168])
-whiteupper = np.array([172,111,255])
-
-
-cyanlower = np.array([85,150,0])
-cyanupper = np.array([95,255,255])
+    whitelower = np.array([0,0,168])
+    whiteupper = np.array([172,111,255])
 
 
-colors = []
+    cyanlower = np.array([85,150,0])
+    cyanupper = np.array([95,255,255])
+
+
+    colors = []
 
 
 # Creating Masks for Colors
 
-redmask = cv2.inRange(img_HSV,redlower,redupper)
-yellowmask = cv2.inRange(img_HSV,yellowlower,yellowupper)
-greenmask = cv2.inRange(img_HSV,greenlower,greenupper)
-bluemask = cv2.inRange(img_HSV,bluelower,blueupper)
-orangemask = cv2.inRange(img_HSV,orangelower,orangeupper)
-blackmask = cv2.inRange(img_HSV,blacklower,blackupper)
-whitemask = cv2.inRange(img_HSV,whitelower,whiteupper)
-cyanmask = cv2.inRange(img_HSV,cyanlower,cyanupper)
+    redmask = cv2.inRange(img_HSV,redlower,redupper)
+    yellowmask = cv2.inRange(img_HSV,yellowlower,yellowupper)
+    greenmask = cv2.inRange(img_HSV,greenlower,greenupper)
+    bluemask = cv2.inRange(img_HSV,bluelower,blueupper)
+    orangemask = cv2.inRange(img_HSV,orangelower,orangeupper)
+    blackmask = cv2.inRange(img_HSV,blacklower,blackupper)
+    whitemask = cv2.inRange(img_HSV,whitelower,whiteupper)
+    cyanmask = cv2.inRange(img_HSV,cyanlower,cyanupper)
 
 
 
 
-if cv2.countNonZero(redmask) > 0 :
-    colors += ["red"]
+    if cv2.countNonZero(redmask) > 0 :
+        colors += ["red"]
 
-if cv2.countNonZero(yellowmask) > 0 :
-    colors+= ["yellow"]
+    if cv2.countNonZero(yellowmask) > 0 :
+        colors+= ["yellow"]
 
-if cv2.countNonZero(greenmask) > 0 :
-    colors+=["green"]
+    if cv2.countNonZero(greenmask) > 0 :
+        colors+=["green"]
 
-if cv2.countNonZero(bluemask) > 0 :
-    colors+=["blue"]
+    if cv2.countNonZero(bluemask) > 0 :
+        colors+=["blue"]
 
-if cv2.countNonZero(orangemask) > 0 :
-    colors+=["orange"]
+    if cv2.countNonZero(orangemask) > 0 :
+        colors+=["orange"]
 
-if cv2.countNonZero(blackmask) > 0 :
-    colors+=["black"]
+    if cv2.countNonZero(blackmask) > 0 :
+        colors+=["black"]
 
-if cv2.countNonZero(whitemask) > 0 :
-    colors+=["white"]
-
-
-
-print(colors)
+    if cv2.countNonZero(whitemask) > 0 :
+        colors+=["white"]
 
 
-plt.imshow(img)
-plt.show()
+
+    print(colors)
 
 
+    plt.imshow(img)
+    plt.show()
+
+show_colours()
 
